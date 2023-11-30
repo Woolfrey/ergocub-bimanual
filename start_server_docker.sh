b4b9715fd7bb04cc90ae073822a94a59e92cd4f4
vigisushrutha23 <vigisushrutha23@gmail.com>
2023-11-30 15:27:54 +0000

    Made changes to launch more stuff from start script

--- start_server_docker.sh
+++ start_server_docker.sh
@@ -28,6 +28,7 @@ tmux rename-window -t  0 'Server'                                               
 # Split Pane 1 to the right, run YARP
 tmux split-window -h
 tmux send-keys    -t $SESSION "yarpserver --write" Enter
+tmux rename-window -t  1 'Yarpserver'                                                        # Give a name to this window
 
 # Split Pane 2, launch Gazebo
 
@@ -35,13 +36,22 @@ if pidof -x "gzserver" >/dev/null; then
     killall -9 gzserver
 fi
 
+# Launching gazebo with custom nav world
 tmux split-window -v
-tmux send-keys -t "export YARP_CLOCK=/clock && YARP_CLOCK=/clock yarprobotinterface --from ecub_yarprobotinterface.ini" Enter
-tmux send-keys    -t $SESSION "gazebo $WORLD -s libgazebo_yarp_clock.so -s libgazebo_ros_init.so " Enter
+tmux rename-window -t  2 'Gazebo'                                                        # Give a name to this window
+tmux select-pane -t 2
+tmux send-keys    -t $SESSION "export YARP_CLOCK=/clock && gazebo -s libgazebo_yarp_clock.so -s libgazebo_ros_init.so $WORLD " Enter 
+              
+#Launching Yarprobotinterface              
+tmux split-window -v
+tmux rename-window -t  3 'Ecubrobotinterface'                                                       
+tmux select-pane -t 3
+tmux send-keys -t $SESSION "export YARP_CLOCK=/clock && YARP_CLOCK=/clock yarprobotinterface --from ecub_yarprobotinterface.ini" Enter
+tmux attach-session -t $SESSION:3                                                        
 
 # Select Pane 0, launch the yarp server
 tmux select-pane -t 0
-tmux send-keys   -t $SESSION "sleep 120" Enter                                             # Wait for Gazebo to launch
+tmux send-keys   -t $SESSION "sleep 12" Enter                                             # Wait for Gazebo to launch
 tmux send-keys   -t $SESSION "$CURRENT_DIR/build/command_server $SERVERNAME $PORT $URDF $CONFIG" Enter
 
 tmux attach-session -t $SESSION:0                                                        # REQUIRED or the above won't execute
